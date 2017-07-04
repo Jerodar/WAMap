0d306a29a3a970cde1d78416d71c5afe053ae8f8
Jerodar <jeroenoostdam@gmail.com>
2017-07-05 00:12:49 +0200

    Added H1. Fixed _processimage to support latest pillow version.

--- processImages/_processImages.py
+++ processImages/_processImages.py
@@ -13,22 +13,23 @@ i = 1
 for infile in filenames:
     print("processing file " + str(i) + " out of " + str(len(filenames)) + ": " + infile)
     f, e = os.path.splitext(infile)
-    outfile1 = "processed/ " + f + ".jpg"
-    outfile2 = "processed/ " + f + "m.jpg"
-    outfile3 = "processed/ " + f + "s.jpg"
+    outfile1 = "processed/" + f + ".jpg"
+    outfile2 = "processed/" + f + "m.jpg"
+    outfile3 = "processed/" + f + "s.jpg"
+    print(outfile1)
     try:
-        Image.open(infile).save(outfile1)
-    except IOError:
-        print("cannot convert", infile)
+        Image.open(infile).convert('RGB').save(outfile1)
+    except IOError as err:
+        print("cannot convert", infile, "because", err)
     try:
-        im = Image.open(infile)
+        im = Image.open(infile).convert('RGB')
         if im.width > mSize:
             im.thumbnail((mSize, im.height * (im.width / mSize)))
         im.save(outfile2)
-    except IOError:
-        print("cannot create thumbnail for", infile)
+    except IOError as err:
+        print("cannot create thumbnail for", infile, "because", err)
     try:
-        im = Image.open(infile)
+        im = Image.open(infile).convert('RGB')
         cropped = im.crop(
             (
                 (im.width - im.height) / 2,
@@ -39,6 +40,6 @@ for infile in filenames:
         )
         cropped.thumbnail((sSize, sSize))
         cropped.save(outfile3)
-    except IOError:
-        print("cannot create square image for", infile)
+    except IOError as err:
+        print("cannot create square image for", infile, "because", err)
     i = i + 1
